cdf0a2f6aff18e518e443bca6d1ebfdb80dd213b
jaehun-jeong <keepthefaith00@gmail.com>
2022-01-18 21:06:12 +0900

    122번 문제 추가

---
+++ src/jh/codetest/Jungol122.java
@@ -0,0 +1,9 @@
+package jh.codetest;
+
+public class Jungol122 {
+    public static void main(String[] args){
+        // 년도를 입력받아 윤년(leap year)인지 평년(common year)인지 판단하는 프로그램을 작성하시오.
+        // http://www.jungol.co.kr/bbs/board.php?bo_table=pbank&wr_id=22&sca=1050
+        
+    }
+}
